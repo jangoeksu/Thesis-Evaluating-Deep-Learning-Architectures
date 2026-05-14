c59ca32957268dbdda067f02fa396056d7b4f83d
XXXXXX <139591259+jangoeksu@users.noreply.github.com>
2026-05-14 11:11:26 +0200

    Update experiment_settings.py

--- configs/experiment_settings.py
+++ configs/experiment_settings.py
@@ -61,11 +61,13 @@ ROBERTA_CONFIG = {
     "checkpoint": "roberta-base",
     "weight_decay": 0.01,
     "logging_strategy": "epoch",
-    "save_strategy": "no",
-    "load_best_model_at_end": False,
+    "save_strategy": "epoch",
+    "load_best_model_at_end": True,
+    "metric_for_best_model": "macro_f1",
+    "greater_is_better": True,
+    "save_total_limit": 1,
     "report_to": "none",
 }
-
 
 AG_LABEL_MAPPING = {
     0: "World",
